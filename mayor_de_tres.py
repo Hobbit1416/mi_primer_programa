92501f35ad53c94fe1a57625186d48d40378696b
Hobbit1416 <emmanuel.mora3415@gmail.com>
2018-03-01 15:11:44 -0500

    my code works, i feel proud of me :D
    the teacher said that he wants another code that allow me to guess the number five times

---
+++ mayor_de_tres.py
@@ -0,0 +1,11 @@
+#
+primer_numero = int( input('Introduzca un numero: ' ))
+
+segundo_numero = int (input('Introduzca um numero: '))
+
+tercer_numero = int(input('Introduzca un numero: ') )
+
+numero_mas_alto = max(primer_numero, segundo_numero, tercer_numero)
+
+
+print(numero_mas_alto)
