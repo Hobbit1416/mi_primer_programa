404ddb17df6e39f1fb16a83df1def497fb56d073
Hobbit1416 <emmanuel.mora3415@gmail.com>
2018-03-01 15:04:14 -0500

    my code works, i feel proud of me :D

---
+++ adivina_numero.py
@@ -0,0 +1,10 @@
+
+numero_a_adivinar = 7
+
+numero_usuario = int(input("dime un numero: "))
+
+if numero_usuario == numero_a_adivinar:
+     print("felicidades, adivinaste el numero, toma una galleta")
+
+else:
+    print("que mal, ese no es, vuelve a intentar")
