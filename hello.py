1efac033e3180fcc2635c2f39aaed001bb473dc7
Hobbit1416 <emmanuel.mora3415@gmail.com>
2018-03-05 08:08:28 -0500

    apetece_helado?

---
+++ hello.py
@@ -0,0 +1,12 @@
+
+
+
+quieres_helado = input ('Te apetece un helado? (Y/N)')
+hay_dinero = input ('Tienes dinero para compralo? (Y/N)')
+esta_el_senor = input ('Esta el se;or de los helados? (Y/N)')
+
+if quieres_helado == 'Y' and hay_dinero == 'Y':
+    print('Bien, pues dale')
+
+else:
+    print('Que pena')
