e1efe18ed643870da89f4504c4ce337ac86fb43f
Hobbit1416 <emmanuel.mora3415@gmail.com>
2018-03-11 17:33:21 -0500

    "farenheit_to_celcius" file added

---
+++ farenheit_to_celcius.py
@@ -0,0 +1,6 @@
+
+faren = int(input('Introduce los grados Farenheit (ºF) a convertir en Celcius (ºC): '))
+
+celcius = (faren - 32)/1.8
+
+print('El resultado en grados celcius (ºC) es: º{}C'.format(celcius))
